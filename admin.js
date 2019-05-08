555ae26970b29cc708e5c2f66f21ef6d82f4c6ff
yangyongqiang <yangyongqiang@czxy.com>
2019-05-08 20:27:37 +0800

    '致命行列无法和原数组同步'

--- admin.js
+++ admin.js
@@ -12,31 +12,40 @@ var Sudoku = {
     'eight': ['','','','','','','','',''],
     'nine': ['','','','','','','','',''],
 };
-
 // 列的列表
-var Line= {
-    'one':[Sudoku.one[0],Sudoku.one[1],Sudoku.one[2],Sudoku.two[0],Sudoku.two[1],Sudoku.two[2],Sudoku.three[0],Sudoku.three[1],Sudoku.three[2]],
-    'two':[Sudoku.one[3],Sudoku.one[4],Sudoku.one[5],Sudoku.two[3],Sudoku.two[4],Sudoku.two[5],Sudoku.three[3],Sudoku.three[4],Sudoku.three[5]],
-    'three':[Sudoku.one[6],Sudoku.one[7],Sudoku.one[8],Sudoku.two[6],Sudoku.two[7],Sudoku.two[8],Sudoku.three[6],Sudoku.three[7],Sudoku.three[8]],
-    'four':[Sudoku.four[0],Sudoku.four[1],Sudoku.four[2],Sudoku.five[0],Sudoku.five[1],Sudoku.five[2],Sudoku.six[0],Sudoku.six[1],Sudoku.six[2]],
-    'five':[Sudoku.four[3],Sudoku.four[4],Sudoku.four[5],Sudoku.five[3],Sudoku.five[4],Sudoku.five[5],Sudoku.six[3],Sudoku.six[4],Sudoku.six[5]],
-    'six':[Sudoku.four[6],Sudoku.four[7],Sudoku.four[8],Sudoku.five[6],Sudoku.five[7],Sudoku.five[8],Sudoku.six[6],Sudoku.six[7],Sudoku.six[8]],
-    'seven':[Sudoku.seven[0],Sudoku.seven[1],Sudoku.seven[2],Sudoku.eight[0],Sudoku.eight[1],Sudoku.eight[2],Sudoku.nine[0],Sudoku.nine[1],Sudoku.nine[2]],
-    'eight':[Sudoku.seven[3],Sudoku.seven[4],Sudoku.seven[5],Sudoku.eight[3],Sudoku.eight[4],Sudoku.eight[5],Sudoku.nine[3],Sudoku.nine[4],Sudoku.nine[5]],
-    'nine':[Sudoku.seven[6],Sudoku.seven[7],Sudoku.seven[8],Sudoku.eight[6],Sudoku.eight[7],Sudoku.eight[8],Sudoku.nine[6],Sudoku.nine[7],Sudoku.nine[8]]
+var Line = {
+    'one': {
+        '0': Sudoku.one[0], 
+        '1': Sudoku.one[1],
+        '2':  Sudoku.one[2],
+        '3':  Sudoku.two[0],
+        '4':  Sudoku.two[1],
+        '5':  Sudoku.two[2],
+        '6':  Sudoku.three[0],
+        '7':  Sudoku.three[1],
+        '8':  Sudoku.three[2]
+    },
+    'two': [Sudoku.one[3], Sudoku.one[4], Sudoku.one[5], Sudoku.two[3], Sudoku.two[4], Sudoku.two[5], Sudoku.three[3], Sudoku.three[4], Sudoku.three[5]],
+    'three': [Sudoku.one[6], Sudoku.one[7], Sudoku.one[8], Sudoku.two[6], Sudoku.two[7], Sudoku.two[8], Sudoku.three[6], Sudoku.three[7], Sudoku.three[8]],
+    'four': [Sudoku.four[0], Sudoku.four[1], Sudoku.four[2], Sudoku.five[0], Sudoku.five[1], Sudoku.five[2], Sudoku.six[0], Sudoku.six[1], Sudoku.six[2]],
+    'five': [Sudoku.four[3], Sudoku.four[4], Sudoku.four[5], Sudoku.five[3], Sudoku.five[4], Sudoku.five[5], Sudoku.six[3], Sudoku.six[4], Sudoku.six[5]],
+    'six': [Sudoku.four[6], Sudoku.four[7], Sudoku.four[8], Sudoku.five[6], Sudoku.five[7], Sudoku.five[8], Sudoku.six[6], Sudoku.six[7], Sudoku.six[8]],
+    'seven': [Sudoku.seven[0], Sudoku.seven[1], Sudoku.seven[2], Sudoku.eight[0], Sudoku.eight[1], Sudoku.eight[2], Sudoku.nine[0], Sudoku.nine[1], Sudoku.nine[2]],
+    'eight': [Sudoku.seven[3], Sudoku.seven[4], Sudoku.seven[5], Sudoku.eight[3], Sudoku.eight[4], Sudoku.eight[5], Sudoku.nine[3], Sudoku.nine[4], Sudoku.nine[5]],
+    'nine': [Sudoku.seven[6], Sudoku.seven[7], Sudoku.seven[8], Sudoku.eight[6], Sudoku.eight[7], Sudoku.eight[8], Sudoku.nine[6], Sudoku.nine[7], Sudoku.nine[8]]
 }
 
 // 行的列表
-var Row= {
-    'one':[Sudoku.one[0],Sudoku.one[3],Sudoku.one[6],Sudoku.four[0],Sudoku.four[3],Sudoku.four[6],Sudoku.seven[0],Sudoku.seven[3],Sudoku.seven[6]],
-    'two':[Sudoku.one[1],Sudoku.one[4],Sudoku.one[7],Sudoku.four[1],Sudoku.four[4],Sudoku.four[7],Sudoku.seven[1],Sudoku.seven[4],Sudoku.seven[7]],
-    'three':[Sudoku.one[2],Sudoku.one[5],Sudoku.one[8],Sudoku.four[2],Sudoku.four[5],Sudoku.four[8],Sudoku.seven[2],Sudoku.seven[5],Sudoku.seven[8]],
-    'four':[Sudoku.two[0],Sudoku.two[3],Sudoku.two[6],Sudoku.five[0],Sudoku.five[3],Sudoku.five[6],Sudoku.eight[0],Sudoku.eight[3],Sudoku.eight[6]],
-    'five':[Sudoku.two[1],Sudoku.two[4],Sudoku.two[7],Sudoku.five[1],Sudoku.five[4],Sudoku.five[7],Sudoku.eight[1],Sudoku.eight[4],Sudoku.eight[7]],
-    'six':[Sudoku.two[2],Sudoku.two[5],Sudoku.two[8],Sudoku.five[2],Sudoku.five[5],Sudoku.five[8],Sudoku.eight[2],Sudoku.eight[5],Sudoku.eight[8]],
-    'seven':[Sudoku.three[0],Sudoku.three[3],Sudoku.three[6],Sudoku.six[0],Sudoku.six[3],Sudoku.six[6],Sudoku.nine[0],Sudoku.nine[3],Sudoku.nine[6]],
-    'eight':[Sudoku.three[1],Sudoku.three[4],Sudoku.three[7],Sudoku.six[1],Sudoku.six[4],Sudoku.six[7],Sudoku.nine[1],Sudoku.nine[4],Sudoku.nine[7]],
-    'nine':[Sudoku.three[2],Sudoku.three[5],Sudoku.three[8],Sudoku.six[2],Sudoku.six[5],Sudoku.six[8],Sudoku.nine[2],Sudoku.nine[5],Sudoku.nine[8]],
+var Row = {
+    'one': [Sudoku.one[0], Sudoku.one[3], Sudoku.one[6], Sudoku.four[0], Sudoku.four[3], Sudoku.four[6], Sudoku.seven[0], Sudoku.seven[3], Sudoku.seven[6]],
+    'two': [Sudoku.one[1], Sudoku.one[4], Sudoku.one[7], Sudoku.four[1], Sudoku.four[4], Sudoku.four[7], Sudoku.seven[1], Sudoku.seven[4], Sudoku.seven[7]],
+    'three': [Sudoku.one[2], Sudoku.one[5], Sudoku.one[8], Sudoku.four[2], Sudoku.four[5], Sudoku.four[8], Sudoku.seven[2], Sudoku.seven[5], Sudoku.seven[8]],
+    'four': [Sudoku.two[0], Sudoku.two[3], Sudoku.two[6], Sudoku.five[0], Sudoku.five[3], Sudoku.five[6], Sudoku.eight[0], Sudoku.eight[3], Sudoku.eight[6]],
+    'five': [Sudoku.two[1], Sudoku.two[4], Sudoku.two[7], Sudoku.five[1], Sudoku.five[4], Sudoku.five[7], Sudoku.eight[1], Sudoku.eight[4], Sudoku.eight[7]],
+    'six': [Sudoku.two[2], Sudoku.two[5], Sudoku.two[8], Sudoku.five[2], Sudoku.five[5], Sudoku.five[8], Sudoku.eight[2], Sudoku.eight[5], Sudoku.eight[8]],
+    'seven': [Sudoku.three[0], Sudoku.three[3], Sudoku.three[6], Sudoku.six[0], Sudoku.six[3], Sudoku.six[6], Sudoku.nine[0], Sudoku.nine[3], Sudoku.nine[6]],
+    'eight': [Sudoku.three[1], Sudoku.three[4], Sudoku.three[7], Sudoku.six[1], Sudoku.six[4], Sudoku.six[7], Sudoku.nine[1], Sudoku.nine[4], Sudoku.nine[7]],
+    'nine': [Sudoku.three[2], Sudoku.three[5], Sudoku.three[8], Sudoku.six[2], Sudoku.six[5], Sudoku.six[8], Sudoku.nine[2], Sudoku.nine[5], Sudoku.nine[8]],
 }
 
 // 定义所需要的变量
@@ -44,44 +53,64 @@ var Row= {
 // temporary 保存当前需要插入的值
 // subscript 随机生成的下标
 // oneNum 保存随机取出来的数字
-var num,temporary,sub;
+var num, temporary, sub;
 // [7,8] 表示第七行 第八列
-var subscript = [];  
+var subscript = [];
 var oneNum = '';
 // 初始化 所有单元格的下标
 var allSubscript = [];
 
-// 生成单元格所有的坐标
-function Initialization(){
-    for(let i= 1;i< 10;i++){
-        for(let j= 1;j< 10;j++){
-            allSubscript.push([i,j]);
+// 生成单元格所有的坐标 ***自调用
+(() => {
+    for (let i = 1; i < 10; i++) {
+        for (let j = 1; j < 10; j++) {
+            allSubscript.push([i, j]);
         }
     }
-}
-Initialization();
+})();
+// 保存单元格内没有插入的元素
+var allCell = {
+    'one': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第一个单元格
+    'two': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第二个单元格
+    'three': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第三个单元格
+    'four': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第四个单元格
+    'five': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第五个单元格
+    'six': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第六个单元格
+    'seven': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第七个单元格
+    'eight': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第八个单元格
+    'nine': [1, 2, 3, 4, 5, 6, 7, 8, 9], //第九个单元格
+};
 // 储存当前下标 所对应的单元格和数组下标
 var grid = [];
 
 // 定义随机生成的数字的数组
-var arr = [1,2,3,4,5,6,7,8,9];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // 随机插入数字的总个数
-var total = 60;
+var total = 81;
+var randomNum  = '';
+
 
 // 第一步 随机取出一个下标  TakeSubscript
-function TakeSubscript(){
+function TakeSubscript() {
     // 随机取出一个下标 这个下边为
-    sub = Math.ceil(Math.random()*allSubscript.length)-1;
-    console.log(sub);
-    console.log(allSubscript.length);
+    sub = Math.ceil(Math.random() * allSubscript.length) - 1;
     subscript.push(allSubscript[sub]);
+
+    // 判断输入那个单元格 -- 属于第几个元素
+    Transformation(subscript[0]);
     
-    oneNum = Math.ceil(Math.random()*8);
+    // 随机生成一个  单元格数字的下标
+    randomNum = Math.ceil(Math.random() * allCell[grid[0]].length);
+    // console.log(grid[0]);
+    oneNum = allCell[grid[0]][randomNum - 1];
+
 }
+
+
 // 把数字换成英文数字
-function replaces(item){
-    switch(item){
+function replaces(item) {
+    switch (item) {
         case 1:
             return 'one';
         case 2:
@@ -101,35 +130,38 @@ function replaces(item){
         case 9:
             return 'nine';
         default:
-            console.log('未知单元格'+item);
-    }
-}
-// 判断要填充的数字是否在 当前单元格内重复
-function palaceIsExist(arr,item){
-    // 当前程序必须是数字
-    item = parseInt(item);
-    // arr必须是一个数组
-    if(typeof arr == 'object'){
-        // 查找当前元素是否存在 不存在返回-1
-        let isExist = arr.indexOf(item);
-        if(isExist=='-1'){
-            // 不存在-表示可以插入
-            return true;
-        }else{
-            // 已经存在 - 应该跳过
-            return false;
-        }
-    }else{
-        console.log('palaceIsExist'+'传入的第一个参数不是一个object数组对象');
+            console.log('未知单元格' + item);
     }
 }
 
+
+// 判断要填充的数字是否在 当前单元格内重复
+function palaceIsExist(arr, item) {
+    // 当前程序必须是数字
+    item = parseInt(item);
+    // arr必须是一个数组
+    if (typeof arr == 'object') {
+        // 查找当前元素是否存在 不存在返回-1
+        let isExist = arr.indexOf(item);
+        if (isExist == '-1') {
+            // 不存在-表示可以插入
+            return true;
+        } else {
+            // 已经存在 - 应该跳过
+            return false;
+        }
+    } else {
+        console.log(palaceIsExist + '传入的第一个参数不是一个object数组对象');
+    }
+}
+
+
 // 根据传过来的参数 判断属于第几个单元格
 function Transformation(item) {
-    if(item[0]<4 && item[1]<4){
+    if (item[0] < 4 && item[1] < 4) {
         grid.push('one');
         item = item.join();
-        switch(item){
+        switch (item) {
             case '1,1':
                 grid.push('0');
                 break;
@@ -159,11 +191,11 @@ function Transformation(item) {
                 break;
         }
         // console.log('第一个单元格');
-    }else if(item[0]> 3 && item[0]< 7 && item[1]< 4){
+    } else if (item[0] > 3 && item[0] < 7 && item[1] < 4) {
         grid.push('four');
         item = item.join();
 
-        switch(item){
+        switch (item) {
             case '4,1':
                 grid.push('0');
                 break;
@@ -193,11 +225,11 @@ function Transformation(item) {
                 break;
         }
         // console.log('第四个单元格');
-    }else if(item[0]> 6  && item[1]< 4 ){
+    } else if (item[0] > 6 && item[1] < 4) {
         grid.push('seven');
         item = item.join();
 
-        switch(item){
+        switch (item) {
             case '7,1':
                 grid.push('0');
                 break;
@@ -227,11 +259,11 @@ function Transformation(item) {
                 break;
         }
         // console.log('第七个单元格');
-    }else if(item[0]< 4 && item[1]> 3 && item[1]< 7){
+    } else if (item[0] < 4 && item[1] > 3 && item[1] < 7) {
         grid.push('two');
         item = item.join();
 
-        switch(item){
+        switch (item) {
             case '1,4':
                 grid.push('0');
                 break;
@@ -261,10 +293,10 @@ function Transformation(item) {
                 break;
         }
         // console.log('第二个单元格');
-    }else if(item[0]> 3 && item[0]< 7 && item[1]> 3 && item[1]< 7){
+    } else if (item[0] > 3 && item[0] < 7 && item[1] > 3 && item[1] < 7) {
         grid.push('five');
         item = item.join();
-        switch(item){
+        switch (item) {
             case '4,4':
                 grid.push('0');
                 break;
@@ -294,10 +326,10 @@ function Transformation(item) {
                 break;
         }
         // console.log('第五个单元格');
-    }else if(item[0]> 6  && item[1]> 3 && item[1]< 7){
+    } else if (item[0] > 6 && item[1] > 3 && item[1] < 7) {
         grid.push('eight');
         item = item.join();
-        switch(item){
+        switch (item) {
             case '7,4':
                 grid.push('0');
                 break;
@@ -327,11 +359,11 @@ function Transformation(item) {
                 break;
         }
         // console.log('第八个单元格');
-    }else if(item[0]< 4  && item[1]> 6){
+    } else if (item[0] < 4 && item[1] > 6) {
         grid.push('three');
         item = item.join();
 
-        switch(item){
+        switch (item) {
             case '1,7':
                 grid.push('0');
                 break;
@@ -361,11 +393,11 @@ function Transformation(item) {
                 break;
         }
         // console.log('第三个单元格');
-    }else if(item[0]> 3 && item[0]< 7 && item[1]> 6 ){
+    } else if (item[0] > 3 && item[0] < 7 && item[1] > 6) {
         grid.push('six');
         item = item.join();
 
-        switch(item){
+        switch (item) {
             case '4,7':
                 grid.push('0');
                 break;
@@ -395,10 +427,10 @@ function Transformation(item) {
                 break;
         }
         // console.log('第六个单元格');
-    }else if(item[0]> 6  && item[1]>6 ){
+    } else if (item[0] > 6 && item[1] > 6) {
         grid.push('nine');
         item = item.join();
-        switch(item){
+        switch (item) {
             case '7,7':
                 grid.push('0');
                 break;
@@ -431,69 +463,70 @@ function Transformation(item) {
     }
 }
 
-for(let i = 0;i<total;i++){
+
+
+for (let i = 0; i < total; i++) {
     // 每次生成把上一次的结果设置为空
     subscript = [];
-    oneNum = '';
+    // oneNum = '';
 
     // 生成随机的下标
     TakeSubscript();
-    // 根据传过来的数字 转换成对应的英文字母 
-    console.log(subscript[0]);
-    let a= replaces(subscript[0][0]);
+    // 根据传过来的数字 转换成对应的英文字母  subscript [[x,x]]
+    let a = replaces(subscript[0][0]);
     // 判断当前行是否允许插入
-    let isLine= palaceIsExist(Line[a],oneNum);
-    if(isLine){
-        // 行能插入
-        let isRow= palaceIsExist(Row[a],oneNum);
-        if(isRow){
+    // console.log(Line[a]);
+    // let isLine = palaceIsExist(Line[a], oneNum);
+    let isLine = true;
+    if (isLine) {
+        // 行能插入  - 判断列是否能插入
+        let isRow = palaceIsExist(Row[a], oneNum);
+        if (isRow) {
             // 列能插入
-            // 判断输入那个单元格
-            Transformation(subscript[0]);
-            let isGrid= palaceIsExist(Sudoku[grid[0]],oneNum);
-            if(isGrid){
-                // 当前单元格是否插入成功  -  并给当前元素赋值
-                console.log(grid+'----'+oneNum);
-                // 判断当前元素是否已经存在元素
-                if(Sudoku[grid[0]][grid[1]] == ''){
-                    Sudoku[grid[0]][grid[1]] = oneNum;
-                    grid = [];
-                    // 删除这个元素
-                    allSubscript.splice(sub,1);
-                }else {
-                    // 当前位置已经有值
-                    console.log(grid[0]+'当前位置已经有值'+Sudoku[grid[0]][grid[1]]);
-                    total++;
-                    grid = [];
-                }
-            }else {
-                // 当前单元格内不能插入元素
-                console.log(grid[0]+'号单元格内已有元素'+oneNum);
-                total++;
-                grid = [];
-
-            }
-        }else {
+           
+            // console.log(randomNum);
+            // console.log('数组的长度' + allCell[grid[0]].length);
+            // 当前单元格是否插入成功  -  并给当前元素赋值
+            // 判断当前元素是否已经存在元素
+            Sudoku[grid[0]][grid[1]] = oneNum;
+            // 删除上一个插入的值
+            allCell[grid[0]].splice(randomNum - 1, 1);
+            // 临时变量设置为空
+            grid = [];
+            // 删除上一个插入的坐标
+            allSubscript.splice(sub, 1);
+            // 把noeNum 设置为空
+            oneNum = '';
+        } else {
             // 列不能插入
             // alert(subscript[1]+'列已有元素'+oneNum);
-            console.log(subscript[0][1]+'列已有元素'+oneNum);
+            console.log(subscript[0][1] + '列已有元素' + oneNum);
             total++;
             grid = [];
+            // 删除上一个插入的坐标
+            allSubscript.splice(sub, 1);
+            // 把noeNum 设置为空
+            oneNum = '';
 
         }
-    }else {
+    } else {
         // 行已经有重复的元素
         // alert(subscript[0]+'行已有元素'+oneNum);
-        console.log(subscript[0][0]+'行已有元素'+oneNum);
+        console.log(subscript[0][0] + '行已有元素' + oneNum);
         total++;
         grid = [];
+        // 删除上一个插入的坐标
+        allSubscript.splice(sub, 1);
+        // 把noeNum 设置为空
+        oneNum = '';
 
     }
 }
+
+
 console.log(Sudoku);
-console.log(allSubscript);
-
-
+console.log(Line);
+console.log(Sudoku.one['0']);
 
 // 遇到问题
 // bug-无法根据传过来的下标 获取这个位置在单元格中的具体位置
@@ -531,4 +564,15 @@ console.log(allSubscript);
 
 // 7.7 7.8 7.9
 // 8.7 8.8 8.9 第九个单元格
-// 9.7 9.8 9.9 
+// 9.7 9.8 9.9
+
+
+// var a=0,b=0;
+// function A(a){
+//     A = function(b){
+//         alert('第二层'+a + b++);
+//     }
+//     alert('第一层'+a++);
+// }
+// A(1);
+// A(2);
